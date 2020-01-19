b3692d1903dca15b84cda09aef68af14e76c447a
Kelly Ann Marshall <kelly@kellyannmarshall.com>
2020-01-19 13:44:30 +0000

    test fix object entries IE11

--- src/components/WhatsNearby.vue
+++ src/components/WhatsNearby.vue
@@ -260,6 +260,16 @@ export default {
         .then(this.isLoading = false)
     },
     getFeatureFormattedFacilities(feature) {
+      if (!Object.entries)
+        Object.entries = function( obj ){
+          var ownProps = Object.keys( obj ),
+              i = ownProps.length,
+              resArray = new Array(i); // preallocate the Array
+          while (i--)
+            resArray[i] = [ownProps[i], obj[ownProps[i]]];
+
+          return resArray;
+      };
       let formattedFacilities = null;
       if (feature.properties.facilities !== null) {
         var lookup = this.labels.product.facilities;
